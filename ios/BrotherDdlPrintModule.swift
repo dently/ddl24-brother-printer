e56663fd9bf1abc8ea9972c1ba9bcf8f1a049bb5
Will Dent <will@dent.ly>
2024-02-09 09:33:52 -0500

    Updates

--- ios/BrotherDdlPrintModule.swift
+++ ios/BrotherDdlPrintModule.swift
@@ -1,44 +1,128 @@
 import ExpoModulesCore
+#if targetEnvironment(simulator)
+  //do nothing
+#else
+  import BRLMPrinterKit
+#endif
 
 public class BrotherDdlPrintModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('BrotherDdlPrint')` in JavaScript.
     Name("BrotherDdlPrint")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
+    AsyncFunction("startSearchWiFiPrinter") { (promise: Promise) in
+      #if targetEnvironment(simulator)
+        promise.resolve("Cannot find printers via wifi when using a simulator.");    
+      #else
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
+        let option = BRLMNetworkSearchOption()
+        option.searchDuration = 15
+        let foundPrinters: NSMutableArray = []
+        let result = BRLMPrinterSearcher.startNetworkSearch(option) { channel in
+            let modelName = channel.extraInfo?.value(forKey: BRLMChannelExtraInfoKeyModelName) as? String ?? ""
+            let ipaddress = channel.channelInfo
+            let serialNumber = channel.extraInfo?.value(forKey: BRLMChannelExtraInfoKeySerialNumber) as? String ?? ""
+            print("modelName: \(modelName) ipaddress: \(ipaddress) serialNumber: \(serialNumber)")
+            //create printerObject
+            let printerObject: NSMutableDictionary = [:]
+            printerObject["modelName"] = modelName
+            printerObject["ipAddress"] = ipaddress
+            printerObject["serialNumber"] = serialNumber
+            printerObject["channelData"] = channel
+            printerObject["type"] = "wifi"
+            foundPrinters.add(printerObject)
+        }
+        promise.resolve(foundPrinters)
+      #endif 
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
+
+
+    AsyncFunction("startSearchBluetoothPrinter") { (promise: Promise) in
+      #if targetEnvironment(simulator)
+        promise.resolve("Cannot find printers via bluetooth when using a simulator.");    
+      #else
+            
+        let foundPrinters: NSMutableArray = []
+        let devices =  BRLMPrinterSearcher.startBluetoothSearch().channels;
+        for channel in devices { 
+            let modelName = channel.extraInfo?.value(forKey: BRLMChannelExtraInfoKeyModelName) as? String ?? ""
+            let ipaddress = channel.channelInfo
+            let serialNumber = channel.extraInfo?.value(forKey: BRLMChannelExtraInfoKeySerialNumber) as? String ?? ""
+            print("modelName: \(modelName) ipaddress: \(ipaddress) serialNumber: \(serialNumber)")
+  
+            //create printerObject
+            let printerObject: NSMutableDictionary = [:]
+            printerObject["modelName"] = modelName
+            printerObject["ipAddress"] = ipaddress
+            printerObject["serialNumber"] = serialNumber
+            printerObject["channelData"] = channel
+            printerObject["type"] = "bluetooth"
+            foundPrinters.add(printerObject)
+        }
+        promise.resolve(foundPrinters);
+      #endif 
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(BrotherDdlPrintView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: BrotherDdlPrintView, prop: String) in
-        print(prop)
+    AsyncFunction("printSamplePDF") {( modelName: String, ipAddress: String, serialNumber: String, printerType: String, strFilePath: String, promise: Promise) in
+        #if targetEnvironment(simulator)
+        let myUrlString = "file:///private/" + strFilePath
+        guard let url = URL(string: myUrlString) else {
+            promise.resolve("Error2 - PDF file is not found.")
+            return
+        }
+          promise.resolve("Cant print pdf \(url)");
+        #else
+        print("modelName: \(modelName) ipAddress: \(ipAddress) serialNumber: \(serialNumber) printerType: \(printerType)");
+        
+          let channel = BRLMChannel(bluetoothSerialNumber: serialNumber)
+
+          let generateResult = BRLMPrinterDriverGenerator.open(channel)
+          guard generateResult.error.code == BRLMOpenChannelErrorCode.noError,
+              let printerDriver = generateResult.driver else {
+                  promise.resolve("Error - Open Channel: \(generateResult.error.code)")
+                  return
+          }
+          defer {
+              printerDriver.closeChannel()
+          }
+        // let url = Bundle.main.url(forResource: "samplepdf2", withExtension: "pdf")
+        // print("url \(url)");
+        
+        
+        let myUrlString = "file:///private/" + strFilePath
+        guard let url = URL(string: strFilePath) else {
+            promise.resolve("Error2 - PDF file is not found.")
+            return
+        }
+        let printerSerieseFromModel = BRLMPrinterClassifier.transferEnum(from:modelName)
+        
+            print("printerSerieseFromModel \(printerSerieseFromModel)")
+            let printSettings = BRLMRJPrintSettings(defaultPrintSettingsWith: BRLMPrinterModel.RJ_4230B)
+//            let printSettings = BRLMTDPrintSettings(defaultPrintSettingsWith: BRLMPrinterModel.RJ_4230B)
+            
+        let defaultPrintSettings = BRLMRJPrintSettings() // Initialize with default values
+
+          // Set your paper information
+          let margins = BRLMCustomPaperSizeMargins(top: 0.12, left: 0.06, bottom: 0.12, right: 0.06)
+
+        let customPaperSize = BRLMCustomPaperSize(dieCutWithTapeWidth: 4, tapeLength: 6, margins: margins, gapLength: 0.125, unitOfLength: .inch)
+            printSettings?.customPaperSize = customPaperSize
+            printSettings?.numCopies = 1
+         
+        let printError = printerDriver.printPDF(with: url, settings: printSettings ?? defaultPrintSettings)
+
+
+
+          if printError.code != .noError {
+              print("Error - Print PDF: \(printError.code)")
+            promise.resolve("Error - Print Image: \(printError.code)")
+          }
+          else {
+            promise.resolve("Success - Print Image")
+          }
+        #endif 
       }
     }
-  }
 }
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import BrotherPrint from "./BrotherPrintModule";
 
-// Import the native module. On web, it will be resolved to BrotherDdlPrint.web.ts
-// and on native platforms to BrotherDdlPrint.ts
-import BrotherDdlPrintModule from './BrotherDdlPrintModule';
-import BrotherDdlPrintView from './BrotherDdlPrintView';
-import { ChangeEventPayload, BrotherDdlPrintViewProps } from './BrotherDdlPrint.types';
-
-// Get the native constant value.
-export const PI = BrotherDdlPrintModule.PI;
-
-export function hello(): string {
-  return BrotherDdlPrintModule.hello();
+export function startSearchWiFiPrinter(): string {
+  console.log('startSearchWiFiPrinter')
+  return BrotherPrint.startSearchWiFiPrinter();
 }
 
-export async function setValueAsync(value: string) {
-  return await BrotherDdlPrintModule.setValueAsync(value);
+export function startSearchBluetoothPrinter() {
+  console.log('startSearchBluetoothPrinter')
+  return BrotherPrint.startSearchBluetoothPrinter();
 }
 
-const emitter = new EventEmitter(BrotherDdlPrintModule ?? NativeModulesProxy.BrotherDdlPrint);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function printSamplePDF(
+  modelName: string,
+  ipAddress: string,
+  serialNumber: string,
+  printerType: string,
+  strFilePath: string,
+) {
+  return BrotherPrint.printSamplePDF(
+    modelName,
+    ipAddress,
+    serialNumber,
+    printerType,
+    strFilePath,
+  );
 }
-
-export { BrotherDdlPrintView, BrotherDdlPrintViewProps, ChangeEventPayload };
